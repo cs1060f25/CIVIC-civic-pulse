c1714937e2e93ca183b73fef3896e8c6eafc6aa9
“gabebrock” <gabebrock04@gmail.com>
2025-10-26 22:51:24 -0400

    feat: add PDF text extraction processor

---
+++ backend/processing/pdf_processor.py
@@ -0,0 +1,15 @@
+import sys, pathlib, pymupdf, os
+
+pdf_dir = r"backend/processing/test_files"
+output_dir = r"backend/processing/output"
+
+# Create output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
+
+for root, dirs, files in os.walk(pdf_dir):
+    for file in files:
+        with pymupdf.open(os.path.join(root, file)) as doc:  # open document
+            text = chr(12).join([page.get_text() for page in doc])
+        
+        # write as a binary file to support non-ASCII characters
+        pathlib.Path(os.path.join(output_dir, file + ".txt")).write_bytes(text.encode())
